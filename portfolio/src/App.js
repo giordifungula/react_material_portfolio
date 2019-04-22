3c9b070add47372b536e8a37e1c56037659cd3e1
giordifungula <jodi.fungula@gmail.com>
2019-04-22 13:57:50 +0200

    added list

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,46 @@
 import React, { Component } from "react";
 import "./App.css";
+import {
+  Layout,
+  Header,
+  Navigation,
+  Drawer,
+  Content,
+  Button,
+  Card,
+  CardText
+} from "react-mdl";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World React app loadded</h1>
+      <div style=" ">
+        <Layout fixedHeader>
+          <Header
+            title={
+              <span>
+                <span style={{ color: "#ddd" }}>Area / </span>
+                <strong>The Title</strong>
+              </span>
+            }
+          >
+            <Navigation>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+            </Navigation>
+          </Drawer>
+          <Content />
+        </Layout>
       </div>
     );
   }
